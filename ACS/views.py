77449a397273e099ad365c324414412b94abc38a
artimre098 <arlkh33m@gmail.com>
2024-03-06 23:03:55 +0800

    user login and logout DOne

--- ACS/views.py
+++ ACS/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from .forms import AddRecordForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm, AddRecordForm
+from .models import Record
 
 # Create your views here.
 
@@ -7,7 +10,30 @@ def home(request):
     return render(request,'home.html',{})
 
 def acs(request):
-    return render(request,'admin/admin-home.html',{})
+    	#Check to see if logging in
+	records = Record.objects.all()
+
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+
+		#authenticate
+		user = authenticate(request, username=username,password=password)
+		if user is not None:
+			login(request,user)
+			messages.success(request,"You have been logged In!")
+			return redirect('acs')
+		else:
+			messages.success(request,"There was an error Logging in, Please try again...")
+			return redirect('acs')
+	else:
+		return render(request,'admin/admin-home.html',{})
+
+def logout_user(request):
+	logout(request)
+	messages.success(request,"You have been Looged Out...")
+	return redirect('acs')
+	
 
 def register(request):
     if request.method == 'POST':
