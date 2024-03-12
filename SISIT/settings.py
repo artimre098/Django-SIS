d873cc43fa8fdcb22dd1b512c09a55d52d580272
Arl Kheem <arlkh33m@gmail.com>
2024-03-12 15:09:40 +0800

    Add .env to .gitignore

--- SISIT/settings.py
+++ SISIT/settings.py
@@ -2,9 +2,7 @@
 from pathlib import Path
 import os
 from decouple import config
-import mimetypes
 
-mimetypes.add_type("text/css", ".css", True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
