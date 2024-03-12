d1af6ae8aa76fec71a8523f0d03d1caffea2e48d
Arl Kheem <arlkh33m@gmail.com>
2024-03-12 14:44:46 +0800

    deploying to railway

--- SISIT/settings.py
+++ SISIT/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR,'collected_static')
 
 STATICFILES_DIRS = [
